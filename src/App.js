06abbae826a8d263686cf728356d071c772c2689
Suman Niroula <sumanniraula00@gmail.com>
2023-03-28 08:06:44 +0545

    changed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import './App.css'
-import Header from './Components/Header'
-import Movie from './Components/Movie'
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Movie from "./Components/Movie";
+import movies from "./Movie.json";
 const App = () => {
   return (
     <div>
       <Header />
-    <div className='main'>
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
-      <Movie />
+      <div className="main">
+        {movies.map((element) => {
+          return(
+          <Movie
+            title={element.Title}
+            year={element.Year}
+            img={element.Poster}
+          
+          />
+          )
+        })}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,11 +1,11 @@
 import React from "react";
 import './Movie.css'
-const Movie = () => {
+const Movie = (props) => {
   return (
     <div className="movie">
-      <img src="https://ia.media-imdb.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg" alt="" />
-      <p>Star Wars: Episode V - The Empire Strikes Back</p>
-      <p>Year:2007</p>
+      <img src={props.img} alt="" />
+      <p>{props.title}</p>
+      <p>{props.year}</p>
     </div>
   );
 };
